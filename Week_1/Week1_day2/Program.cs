4b5eea78aaf753045488def6b0511a165c7b63e6
mhieu <nguyenminhhieu1252005@gmail.com>
2026-07-19 21:55:09 +0700

    excercise_week1

---
+++ Week_1/Week1_day2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tinhdiemtrungbinh
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input your point:");
+            float yourPoint = float.Parse(Console.ReadLine());
+            while (true)
+            {
+                if (yourPoint < 0 || yourPoint > 10)
+                {
+                    Console.WriteLine("Your point is invalid,please try again! ");
+                    yourPoint = float.Parse(Console.ReadLine());
+                }
+                else break;
+            }
+
+            if(yourPoint < 5.0)
+            {
+                Console.WriteLine("Fail");
+            }
+            else if(yourPoint <7.0)
+            {
+                Console.WriteLine("Average");
+            }
+            else if(yourPoint <8.0)
+            {
+                Console.WriteLine("Good");
+            }
+            else
+            {
+                Console.WriteLine("Very good");
+            }
+
+        }
+    }
+}

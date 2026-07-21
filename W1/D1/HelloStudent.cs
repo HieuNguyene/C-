e6247aa09b6cd1058159f3c7be2cda33d0572fef
mhieu <nguyenminhhieu1252005@gmail.com>
2026-07-21 21:49:15 +0700

    w1_full

---
+++ W1/D1/HelloStudent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1.D1
+{
+    
+    internal class HelloStudent
+    {
+        public void Excute()
+        {
+            Console.WriteLine("Welcome, please fill in your information!");
+            Console.WriteLine("Your name:");
+            string? name;
+            do
+            {
+                name = Console.ReadLine();
+            } while (string.IsNullOrWhiteSpace(name));
+
+            Console.WriteLine("Your student id:");
+            string? msv = Console.ReadLine();
+            Console.WriteLine("Your age:");
+            int age;
+            while (true)
+            {
+                var ageInput = Console.ReadLine();
+                if (!int.TryParse(ageInput, out age) || age <= 0 || age >= 100)
+                {
+                    Console.WriteLine("Your age invalid, please try again!");
+                }
+                else break;
+            }
+            Console.WriteLine($"Welcome {name}-{msv}-{age}");
+        }
+    }
+}
---
+++ W1/D2/AverageScore.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1.D2
+{   
+    internal class AverageScore
+    {
+        public void Excute()
+        {
+            Console.WriteLine("Please input your point:");
+            float yourPoint;
+            while (true)
+            {
+                if (!float.TryParse(Console.ReadLine(), out yourPoint) || yourPoint < 0 || yourPoint > 10)
+                {
+                    Console.WriteLine("Your point is invalid,please try again! ");
+                }
+                else break;
+            }
+
+            if (yourPoint < 5.0)
+            {
+                Console.WriteLine("Fail");
+            }
+            else if (yourPoint < 7.0)
+            {
+                Console.WriteLine("Average");
+            }
+            else if (yourPoint < 8.0)
+            {
+                Console.WriteLine("Good");
+            }
+            else
+            {
+                Console.WriteLine("Very good");
+            }
+
+        }
+
+    }
+}
+                
+
+
